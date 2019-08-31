7f9042dfe987d8727f1010ccae06260246723091
Namugumya Sandra <salyn134@gmail.com>
2019-08-31 11:48:53 +0300

    Edit comparision operator

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,7 @@ function fizzBuzz(list1, list2) {
     // Your code here
     let newList = list1.concat(list2);
     let totalLength = newList.length;
-    if(totalLength % 3 == 0 && totalLength % 5 == 0){
+    if(totalLength % 3 === 0 && totalLength % 5 === 0){
         return('Fizzbuzz');
     } else if(totalLength % 5 === 0){
         return('Buzz');
