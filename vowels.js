70dc085b69b6047467acae6b863133fe5c69e56a
Namugumya Sandra <salyn134@gmail.com>
2019-09-09 23:43:19 +0300

    Add repeat letter count.

--- vowels.js
+++ vowels.js
@@ -1,20 +1,57 @@
-
-function countVowels(word){
-    var vowels = ['a','e','i','o','u'];
-    if(typeof word === 'string'){
-        var wordArray = Array.from(word.toLowerCase());
-        var cleanWordArray = [];
-        for(var i in vowels ){
-            if (wordArray.includes(vowels[i]) !== false){
-                cleanWordArray.push(vowels[i]);
-            }
+var vowels = ['a','e','i','o','u'];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//function to filter the vowels
+function wordVowels(x){
+    var cleanWordArray = [];
+    for(var i in vowels ){
+        if (x.includes(vowels[i]) !== false){
+            cleanWordArray.push(vowels[i]);
         }
-        console.log(cleanWordArray.join(''));
     }
-
+    return cleanWordArray.join('');
 };
 
-countVowels('adfdghyuresxcv');
-countVowels('drinik water');
+//function to get duplicates count 
+var newWord = [];
+var newWordArray = [];  
+function getRepeatedLetters(y){
+    var newArray = [];
+  //create arrray of all values as letters
+    for(var i in y ){
+        if (letters.includes(y[i]) !== false){
+            newArray.push(y[i]);
+    }
+  }
+  //sort out the repeated values in the array
+    for(var l in newArray){
+        if (newArray.indexOf(newArray[l]) !== newArray.lastIndexOf(newArray[l])){
+            newWord.push(newArray[l]);
+        }
+   }
+  //function to filter duplicates
+    var arrayUnique = function (arr) {
+        return arr.filter(function(item, index){
+            return arr.indexOf(item) >= index;
+    });
+  };
+  return arrayUnique(newWord).length;
+}
 
-// module.exports = countVowels
+
+
+// function to return vowels and duplicates count
+function countVowels(word){
+    var vowelWord;
+    var repeatCount;
+    if(typeof word === 'string'){
+        var wordArray = Array.from(word.toLowerCase());
+        vowelWord = wordVowels(wordArray);
+        repeatCount = getRepeatedLetters(wordArray);
+        return `("${vowelWord}",${repeatCount})`;
+    }
+};
+// countVowels('drink water');
+// countVowels('dahdah');
+// countVowels('adafgdhyure');
+
+module.exports = countVowels
