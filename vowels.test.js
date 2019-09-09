1c8c5bd016966216c23864fc224c085e1309a9f9
Namugumya Sandra <salyn134@gmail.com>
2019-09-09 23:44:32 +0300

    Test the countVowels.

--- vowels.test.js
+++ vowels.test.js
@@ -1,15 +1,19 @@
 const countVowels = require('./vowels');
 
-test('returns ("a",3) when input string is "dahdah" ', () => {
-    expect(countVowels('dahdah')).toBe('a',3);
-});
-
-test('returns ("iae",1) when input string is "drink water" ', () => {
-    expect(countVowels('drink water')).toBe('iae',1);
+it('returns ("a",3) when input string is "dahdah" ', () => {
+    expect(countVowels('dahdah')).toBe('("a",3)');
 });
 
 test('returns ("a",3) when input string is "DAHDAH" ', () => {
-    expect(countVowels('DAHDAH')).toBe('a',3);
+    expect(countVowels('DAHDAH')).toBe('("a",3)');
+});
+
+test('returns ("a",3) when input string is "DAHDA4H" ', () => {
+    expect(countVowels('DAHDA4H')).toBe('("a",3)');
+});
+
+test('returns ("a",3) when input string is "DAHDA$H" ', () => {
+    expect(countVowels('DAHDA$H')).toBe('("a",3)');
 });
 // countVowels(‘dahdah’) # will return (‘a’, 3)
 // countVowels(‘drink water’) # will return (‘iae’, 1)
