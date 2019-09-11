3198093d18a8fba6e6985f688257dabbcfcf3932
Namugumya Sandra <salyn134@gmail.com>
2019-09-11 21:55:55 +0300

    Add test case for invalid inputs.

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,13 @@
 const fizzBuzz = require('./fizzbuzz');
 
+test('returns Invalid when one of the inputs is a string', () => {
+    expect(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8], "fixxsft")).toBe('Invalid input.');
+});
+
+test('returns Invalid when one of the inputs is a number', () => {
+    expect(fizzBuzz(234,[1, 2, 3, 4, 5, 6, 7, 8])).toBe('Invalid input.');
+});
+
 test('returns Fizzbuzz when sum of length is divisible by 3 and 5 ', () => {
     expect(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7])).toBe('Fizzbuzz');
 });
